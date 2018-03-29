87293addddd03571a079ef84327e08679a843f22
charliecek <charliecek@gmail.com>
2018-03-29 18:37:46 +0200

    Stub of the login action after successful registration

--- js/florp_nf_action_controller.js
+++ js/florp_nf_action_controller.js
@@ -10,6 +10,7 @@ function add_florp_submit_controller() {
       // On the Form Submission's field validaiton...
       var submitChannel = Backbone.Radio.channel( 'submit' );
       this.listenTo( submitChannel, 'validate:field', this.validateSubmit);
+      this.listenTo( Backbone.Radio.channel( 'forms' ), 'submit:response', this.actionSubmit );
 
       // other validation types: http://developer.ninjaforms.com/codex/client-side-field-validation/
     },
@@ -33,6 +34,50 @@ function add_florp_submit_controller() {
           var error_id = error.get('id');
           this.formModel.removeError(error_id);
         }
+      }
+    },
+    
+     actionSubmit: function( response ) {
+      // Do stuff here.
+      console.log(response);
+      var errors = this.formModel.get( 'errors' );
+      if (errors.length == 0) {
+        console.log("Success");
+        // response.data.fields_by_key -> use to login
+//         var successMessageSpan = jQuery(".lwa-status.lwa-status-confirm");
+//         var successMessage = successMessageSpan.html();
+//         
+//         // wait .1 sec before showing the success message //
+//         setTimeout(function() {
+//           successMessageSpan.html(successMessage + " " + data.loggingInMsg);
+//           
+//           // wait 3 sec before filling in the login form //
+//           setTimeout(function() {
+//             var idPrefix = form.data("idPrefix");
+//             var loginForm = jQuery("form.lwa-form");
+//             var loginUsernameInput = jQuery("#"+idPrefix+"lwa_user_login");
+//             var loginPasswordInput = jQuery("#"+idPrefix+"lwa_user_pass");
+//             if (idPrefix.length === 0) {
+//               var cancelButton = jQuery(".lwa-links-register-inline-cancel.button");
+//             }
+//             var loginUsername = response.data.fields_by_key.user_name.value;
+//             var loginPassword = response.data.fields_by_key.user_pass.value;
+//             
+//             loginUsernameInput.val(loginUsername);
+//             loginPasswordInput.val(loginPassword);
+//             
+//             // hide registration form //
+//             if (idPrefix.length === 0) {
+//               cancelButton.click();
+//             }
+//             
+//             // wait .1 sec before submitting the login form
+//             setTimeout(function() {
+//               // submit login form //
+//               loginForm.submit();
+//             }, 100);
+//           }, 3000);
+//         }, 100);
       }
     },
     
